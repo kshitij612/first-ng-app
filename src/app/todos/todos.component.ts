2e611b2aa8a6767547f45c5f32478167b81b3184
Kshitij Kumar <kshitijs8789@gmail.com>
2025-01-10 23:34:50 +0530

    added directives for checkbox responses and highlighting, added custom pipe for searching todos

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -2,16 +2,20 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { TodosService } from '../services/todo.service';
 import { Todo } from '../model/todo.type';
 import { catchError } from 'rxjs';
+import { TodoItemComponent } from '../components/todo-item/todo-item.component';
+import { FormsModule } from '@angular/forms';
+import { FilterTodosPipe } from '../pipes/filter-todos.pipe';
 
 @Component({
   selector: 'app-todos',
-  imports: [],
+  imports: [TodoItemComponent,FormsModule,FilterTodosPipe],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss'
 })
 export class TodosComponent implements OnInit {
   todoService = inject(TodosService);
   todoItems = signal<Array<Todo>>([]);
+  searchTerm = signal('');
 
   ngOnInit(): void {
     this.todoService
@@ -25,5 +29,19 @@ export class TodosComponent implements OnInit {
       .subscribe((todos) => {
         this.todoItems.set(todos);
       });
+  }
+
+  updateTodoItem(todoItem: Todo) {
+    this.todoItems.update((todos) => {
+      return todos.map(todo => {
+        if (todo.id == todoItem.id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      })
+    })
   }
 }
